8080a8c65faffe480c87aa78c9692832f0a98815
Nikolai <Drob_mf41@student.itstep.org>
2023-03-11 23:29:19 +0200

    Add migration

---
+++ app/Models/Author.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Author extends Model
+{
+    use HasFactory;
+    protected $table = 'authors';
+    public function books(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(Book::class);
+    }
+}
